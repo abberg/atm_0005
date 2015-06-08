0ed3a6a4bd6a7684e15c47ccc998ba3bc183e884
Andrew Berg <andrewbberg@gmail.com>
2015-06-08 10:09:30 -0400

    setup disks and floating ball

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -5,43 +5,103 @@
 		var scene = three.scene(),
 			camera = three.camera(),
 			renderer = three.renderer(),
-			container,
-			model,
-			previousModel,
+			clock = new THREE.Clock(true),
+			pointLight,
+			imposter,
+			composer,
 
 			init = function(){
-				var geometry = new THREE.BoxGeometry( 1, 1, 1 ),
-					material = new THREE.MeshLambertMaterial( { color: 0xffffff } ),
-					mesh = new THREE.Mesh( geometry, material ),
-					directionalLight = new THREE.DirectionalLight( 0xffffff );
-
-				container = new THREE.Object3D();
-				model = new THREE.Object3D();
-				previousModel = model.clone();
-				
-				container.add(mesh);
-				scene.add( container );
-
-				scene.add(directionalLight);
-				directionalLight.position.set( 1, 1, 1 );
-
-				renderer.setClearColor( 0x333333 );
-
-				camera.position.z = 3;
+				renderer.setClearColor(0x09181C);
+				addLights();
+				addMeshes();
+				setupPostProcessing();
+				camera.position.set(0, 0, 15);
 			},
 			
+			addLights = function(){
+				var keyLight,
+					bounceLight;
+
+				keyLight = new THREE.DirectionalLight(0xffffff, 0.7);
+				keyLight.position.set(0, 1, 0);
+				scene.add(keyLight);
+
+				bounceLight = new THREE.DirectionalLight(0xaaffff, 0.3);
+				bounceLight.position.set(0, -1, 0);
+				scene.add(bounceLight);
+
+				pointLight = new THREE.PointLight(0xffffff, 0.5);
+				scene.add(pointLight);
+
+			},
+
+			addMeshes = function(){
+				var geometry,
+				material,
+				mesh,
+				occlusionMesh,
+				i;
+
+				for(i = -1; i < 2; i++){
+					
+					geometry = new THREE.TorusGeometry( 5, 1.5, 12, 64);
+					material = new THREE.MeshPhongMaterial( { color: 0x777777, shininess:100} );
+					mesh = new THREE.Mesh( geometry, material );
+					
+					mesh.position.y = i * 4;
+					mesh.rotation.x = Math.PI / 2;
+					mesh.scale.z = 0.3;
+					scene.add( mesh );
+
+					geometry = new THREE.TorusGeometry( 6.45, 0.1, 12, 64);
+					material = new THREE.MeshBasicMaterial( { color: 0xaaffff} );
+					mesh = new THREE.Mesh( geometry, material );
+					
+					mesh.position.y = i * 4;
+					mesh.rotation.x = Math.PI / 2;
+					scene.add( mesh );
+
+					geometry = new THREE.TorusGeometry( 5, 0.15, 12, 64);
+					material = new THREE.MeshBasicMaterial( { color: 0xaaffff} );
+					mesh = new THREE.Mesh( geometry, material );
+					
+					mesh.position.y = ( i * 4 ) + 0.45;
+					mesh.rotation.x = Math.PI / 2;
+					scene.add( mesh );
+				
+				}
+
+				geometry = new THREE.IcosahedronGeometry( 1.5, 2 );
+				material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+				imposter = new THREE.Mesh( geometry, material );
+				scene.add(imposter);
+
+			},
+
+			setupPostProcessing = function(){
+				renderer.autoClear = false;
+
+				composer = new THREE.EffectComposer( renderer );
+				composer.addPass( new THREE.RenderPass( scene, camera ) );
+
+				var bloomEffect = new THREE.BloomPass(0.8);
+				composer.addPass( bloomEffect );
+				
+				var effectFXAA = new THREE.ShaderPass( THREE.FXAAShader );
+  				effectFXAA.uniforms.resolution.value.set( 1 / renderer.domElement.width, 1 / renderer.domElement.height );
+  				window.addEventListener('resize', function(){
+					effectFXAA.uniforms.resolution.value.set( 1 / renderer.domElement.width, 1 / renderer.domElement.height );
+				});
+				effectFXAA.renderToScreen = true;
+				composer.addPass( effectFXAA );
+			},
+
 			update = function(timestep){
-				var rotationVelocity = 0.001;
-				
-				model.clone(previousModel);
-				model.rotation.x += rotationVelocity * timestep;
-				model.rotation.y += rotationVelocity * timestep;
-				
+				pointLight.position.y = imposter.position.y = Math.sin(clock.getElapsedTime()/2) * 6;
 			},
 			
 			draw = function(interpolation){
-				THREE.Quaternion.slerp ( previousModel.quaternion, model.quaternion, container.quaternion, interpolation );
-				renderer.render(scene, camera);
+				composer.render();
 			}
 
 		return{
